ad56068e798fe0d4eaa82839f4cf9be2ab6e29b0
Dikz <cicerogabriel12@hotmail.com>
2022-01-29 11:20:28 -0300

    PWA config

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+    // "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    // "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react",
+  "features": {
+    "postcss": false
+  }
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um Atomo e React Avançado ao Lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illutstration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para tela de código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
